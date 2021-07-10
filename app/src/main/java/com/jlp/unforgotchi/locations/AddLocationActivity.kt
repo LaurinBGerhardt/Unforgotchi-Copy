7667a7c12fd19dbdbfec4e119e6b2cac24425920
Paul Felkner <p.felkner@campus.lmu.de>
2021-07-10 15:40:26 +0200

    changes due to merge conflict

--- app/src/main/java/com/jlp/unforgotchi/locations/AddLocationActivity.kt
+++ app/src/main/java/com/jlp/unforgotchi/locations/AddLocationActivity.kt
@@ -5,12 +5,11 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.net.wifi.SupplicantState
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +17,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
+import com.jlp.unforgotchi.MainActivity
 import com.jlp.unforgotchi.R
 import com.jlp.unforgotchi.db.ReminderList
 import com.jlp.unforgotchi.db.ReminderListViewModel
@@ -33,6 +33,8 @@ class AddLocationActivity : AppCompatActivity() {
     private val previewImage by lazy { findViewById<ImageButton>(R.id.selected_location_image_button) }
     private var previewImageChanged : Boolean = false   //this is horrible coding dont copy this
     private var imageData : Uri? = null
+    private val addWifiButton : Button by lazy { findViewById<Button>(R.id.add_wifi_to_location_button) }
+    private var wifiName : String? = null
 
     //private val selectImageFromGalleryResult  = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
     private val selectImageFromGalleryResult  = registerForActivityResult(RetreiveImageContract()) { uri: Uri? ->
@@ -57,28 +59,22 @@ class AddLocationActivity : AppCompatActivity() {
         setContentView(R.layout.add_location_layout)
         addLocNameView = findViewById(R.id.add_name_of_location)
         previewImage.setImageResource(R.drawable.ic_baseline_image_search_24)
-        wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
-        wifiInfo = wifiManager.connectionInfo
         previewImage.setOnClickListener {
             selectImageFromGallery()
         }
 
-        val reminderListsVM: ReminderListViewModel = ViewModelProvider(this).get(ReminderListViewModel::class.java)
+        addWifiButton.setOnClickListener{
+            wifiName = MainActivity.getSsid(this)
+        }
 
-        // fill the 'spinner_items' array with all items to show
-        //val allObjects: List<DropdownListElement> = getMyObjects() // from wherever
-        //val currentLists : List<ReminderList> = reminderListsVM.readCurrentData()
-        //for (list in currentLists) {
-        //    dropdownItems.add(DropDownAdapter.DropDownItem(list, list.listName))
-        //}
+        //This is for the Dropdown Menu:
+        val reminderListsVM: ReminderListViewModel = ViewModelProvider(this).get(ReminderListViewModel::class.java)
         reminderListsVM.readAllData.observe(this, Observer { reminderLists ->
             for (reminderList in reminderLists) {
                 dropdownItems.add(DropDownAdapter.DropDownItem(reminderList, reminderList.listName))
             }
         })
-
         val headerText = "Select a List"
-
         val spinner: Spinner = findViewById(R.id.select_lists_spinner)
         val adapter = DropDownAdapter(this, headerText, dropdownItems, selectedLists)
         spinner.adapter = adapter
@@ -86,19 +82,11 @@ class AddLocationActivity : AppCompatActivity() {
         // From here it should be possible to just see what's inside selectedLists
         // and work with that
 
+        //Do this last!:
         findViewById<FloatingActionButton>(R.id.finish_adding_location).setOnClickListener {
             processInput()
         }
 
-    }
-
-    private fun getSsid(): String? {
-        if (wifiInfo.supplicantState == SupplicantState.COMPLETED) {
-            Log.d("SSID: ", wifiInfo.ssid)
-            return wifiInfo.ssid
-        } else {
-            return null
-        }
     }
 
     private fun processInput() {
@@ -110,11 +98,13 @@ class AddLocationActivity : AppCompatActivity() {
             previewImage.setImageResource(R.drawable.ic_baseline_location_city_24)
             val name = addLocNameView.text.toString()
             intent.putExtra("name", name)
+            intent.putExtra("wifiName", wifiName)
             setResult(Activity.RESULT_OK, intent)
         } else {
             val name = addLocNameView.text.toString()
             intent.putExtra("name", name)
             intent.putExtra("image",imageData)
+            intent.putExtra("wifiName", wifiName)
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
             intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
             intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
@@ -123,7 +113,6 @@ class AddLocationActivity : AppCompatActivity() {
                 Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION)
             setResult(Activity.RESULT_OK, intent)
         }
-        getSsid()
         finish()
     }
 
